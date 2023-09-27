12f8b452fbc36385eabba56a5d2e092766fd748b
Galina Matvienko <galyagmgm@gmail.com>
2023-09-27 14:13:16 +0200

    mm

--- server/build/router/users.js
+++
@@ -1,9 +0,0 @@
-import { deleteUser, getAllUsers, getUserId, updateUser, } from "../controllers/users.js";
-import { authenticateToken } from "../middlwares/authMiddlware.js";
-export default (router) => {
-    router.get("/users", authenticateToken, getAllUsers);
-    router.get("/users/:id", getUserId);
-    router.delete("/users/:id", deleteUser);
-    router.patch("/users/:id", updateUser);
-};
-//# sourceMappingURL=users.js.map
