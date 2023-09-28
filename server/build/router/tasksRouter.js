5ca84772f1358244e51d47c9124978a42bdd9ddb
Galina Matvienko <galyagmgm@gmail.com>
2023-09-28 11:06:12 +0200

    hj

--- server/build/router/tasksRouter.js
+++
@@ -1,9 +0,0 @@
-import { createNewTask, getTasks, getTaskId, deleteTask, updateTask, } from "../controllers/tasks.js";
-export default (router) => {
-    router.post("/tasks/", createNewTask);
-    router.get("/tasks/", getTasks);
-    router.get("/tasks/:id", getTaskId);
-    router.delete("/tasks/:id", deleteTask);
-    router.put("/tasks/:id", updateTask);
-};
-//# sourceMappingURL=tasksRouter.js.map
